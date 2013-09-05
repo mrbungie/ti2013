fa3ea2d0f465d3ea5930468b670bf5be96ebcbe3
German Oviedo <geroa220@gmail.com>
2013-09-05 02:42:21 -0400

    Arregladas comillas

--- Prueba01-2012_v1/1/1a.php
+++ Prueba01-2012_v1/1/1a.php
@@ -1,21 +1,21 @@
 <?php
 
-mysql_connect(“localhost”,”root”,””);
-mysql_select_db(“une”);
-$consulta = “SELECT nombre, apellido, reprobados, promedio FROM alumnos ORDER BY apellido DESC”;
+mysql_connect('localhost','root','');
+mysql_select_db('une');
+$consulta = "SELECT nombre, apellido, reprobados, promedio FROM alumnos ORDER BY apellido DESC";
 $resultado = mysql_query($consulta);
 $fila = mysql_fetch_assoc($resultado);
 while($fila != null){
-	$promedio = $fila[‘promedio’];
-	$reprobados = $fila[‘reprobados’]
-	echo $fila[‘nombre’].’ ‘.$fila[‘apellido’].’ – ‘;
+	$promedio = $fila['promedio'];
+	$reprobados = $fila['reprobados']
+	echo $fila['nombre'].' '.$fila['apellido'].' – ';
 	// Recordar: && es Y, mientras que || es O. 
 	// Por otro lado, "or" y "and" también sirven.
 	if( ($promedio >= 5.5 && $reprobados == 0) || ($promedio >= 6.0 && $reprobados <= 1) || ($promedio >= 6.5 && reprobados <= 2) ) 
-		echo “BECADO”;
+		echo 'BECADO';
 	else 
-		echo “NO BECADO”;
-	echo “<br>”;
+		echo 'NO BECADO';
+	echo '<br>';
 	$fila = mysql_fetch_assoc($resultado);
 }
 
--- Prueba01-2012_v1/1/1bbuscar.php
+++ Prueba01-2012_v1/1/1bbuscar.php
@@ -1,22 +1,24 @@
 <?php
 
-mysql_connect(“localhost”,”root”,””);
-mysql_select_db(“une”);
+mysql_connect('localhost','root','');
+mysql_select_db('une');
+// Las unicas diferencias con la 1a son las siguientes lineas:
 $busqueda = $_REQUEST['apellido'];
-$consulta = “SELECT nombre, apellido, reprobados, promedio FROM alumnos WHERE apellido LIKE '%busqueda%' ORDER BY apellido DESC”;
+$consulta = "SELECT nombre, apellido, reprobados, promedio FROM alumnos WHERE apellido LIKE '%busqueda%' ORDER BY apellido DESC";
+// Fin diferencias
 $resultado = mysql_query($consulta);
 $fila = mysql_fetch_assoc($resultado);
 while($fila != null){
-	$promedio = $fila[‘promedio’];
-	$reprobados = $fila[‘reprobados’]
-	echo $fila[‘nombre’].’ ‘.$fila[‘apellido’].’ – ‘;
+	$promedio = $fila['promedio'];
+	$reprobados = $fila['reprobados']
+	echo $fila['nombre'].' '.$fila['apellido'].' – ';
 	// Recordar: && es Y, mientras que || es O. 
 	// Por otro lado, "or" y "and" también sirven.
 	if( ($promedio >= 5.5 && $reprobados == 0) || ($promedio >= 6.0 && $reprobados <= 1) || ($promedio >= 6.5 && reprobados <= 2) ) 
-		echo “BECADO”;
+		echo 'BECADO';
 	else 
-		echo “NO BECADO”;
-	echo “<br>”;
+		echo 'NO BECADO';
+	echo '<br>';
 	$fila = mysql_fetch_assoc($resultado);
 }
 
