dfeb15ca14e2c9239226bf236005b14fa69588a2
mrbungie <geroa220@gmail.com>
2013-09-11 19:28:44 -0400

    Signos peso y DESC

--- Prueba01-2012_v1/1/1bbuscar.php
+++ Prueba01-2012_v1/1/1bbuscar.php
@@ -4,7 +4,7 @@ mysql_connect('localhost','root','');
 mysql_select_db('une');
 // Las unicas diferencias con la 1a son las siguientes lineas:
 $busqueda = $_REQUEST['apellido'];
-$consulta = "SELECT nombre, apellido, reprobados, promedio FROM alumnos WHERE apellido LIKE '%busqueda%' ORDER BY apellido DESC";
+$consulta = "SELECT nombre, apellido, reprobados, promedio FROM alumnos WHERE apellido LIKE '%$busqueda%' ORDER BY apellido";
 // Fin diferencias
 $resultado = mysql_query($consulta);
 $fila = mysql_fetch_assoc($resultado);
@@ -14,7 +14,7 @@ while($fila != null){
 	echo $fila['nombre'].' '.$fila['apellido'].' – ';
 	// Recordar: && es Y, mientras que || es O. 
 	// Por otro lado, "or" y "and" también sirven.
-	if( ($promedio >= 5.5 && $reprobados == 0) || ($promedio >= 6.0 && $reprobados <= 1) || ($promedio >= 6.5 && reprobados <= 2) ) 
+	if( ($promedio >= 5.5 && $reprobados == 0) || ($promedio >= 6.0 && $reprobados <= 1) || ($promedio >= 6.5 && $reprobados <= 2) ) 
 		echo 'BECADO';
 	else 
 		echo 'NO BECADO';
